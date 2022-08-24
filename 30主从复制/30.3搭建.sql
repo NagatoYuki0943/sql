2720eb9f3c7a46bfdbb7004635e77eb69ba10d99
P775TM1 <lihongtu12138@outlook.com>
2022-08-24 18:25:20 +0800

    主从复制在重启系统后仍然生效,二进制文件和位置自动会更新

--- 30主从复制/30.3搭建.sql
+++ 30主从复制/30.3搭建.sql
@@ -237,3 +237,121 @@
         +----+---------+------+
         3 rows in set (0.00 sec)
 
+
+    3.重启两台服务器尝试效果
+        主库:
+            show master status;
+            +---------------+----------+--------------+------------------+-------------------+
+            | File          | Position | Binlog_Do_DB | Binlog_Ignore_DB | Executed_Gtid_Set |
+            +---------------+----------+--------------+------------------+-------------------+
+            | binlog.000011 |      156 |              |                  |                   |
+            +---------------+----------+--------------+------------------+-------------------+
+            1 row in set (0.00 sec)
+
+        从库:
+            show replica status \G;
+            *************************** 1. row ***************************
+                        Replica_IO_State: Waiting for source to send event
+                            Source_Host: 172.17.0.2
+                            Source_User: itcast
+                            Source_Port: 3306
+                            Connect_Retry: 60
+                        Source_Log_File: binlog.000011      -- 自动更新到000011
+                    Read_Source_Log_Pos: 156
+                        Relay_Log_File: 645e64df4888-relay-bin.000008
+                            Relay_Log_Pos: 365
+                    Relay_Source_Log_File: binlog.000011
+                    Replica_IO_Running: Yes
+                    Replica_SQL_Running: Yes
+                        Replicate_Do_DB:
+                    Replicate_Ignore_DB:
+                    Replicate_Do_Table:
+                Replicate_Ignore_Table:
+                Replicate_Wild_Do_Table:
+            Replicate_Wild_Ignore_Table:
+                            Last_Errno: 0
+                            Last_Error:
+                            Skip_Counter: 0
+                    Exec_Source_Log_Pos: 156                -- 条目也更新
+                        Relay_Log_Space: 790
+                        Until_Condition: None
+                        Until_Log_File:
+                            Until_Log_Pos: 0
+                    Source_SSL_Allowed: No
+                    Source_SSL_CA_File:
+                    Source_SSL_CA_Path:
+                        Source_SSL_Cert:
+                        Source_SSL_Cipher:
+                        Source_SSL_Key:
+                    Seconds_Behind_Source: 0
+            Source_SSL_Verify_Server_Cert: No
+                            Last_IO_Errno: 0
+                            Last_IO_Error:
+                        Last_SQL_Errno: 0
+                        Last_SQL_Error:
+            Replicate_Ignore_Server_Ids:
+                        Source_Server_Id: 1
+                            Source_UUID: cb08b6fb-1b07-11ed-856a-0242ac110002
+                        Source_Info_File: mysql.slave_master_info
+                                SQL_Delay: 0
+                    SQL_Remaining_Delay: NULL
+                Replica_SQL_Running_State: Replica has read all relay log; waiting for more updates
+                    Source_Retry_Count: 86400
+                            Source_Bind:
+                Last_IO_Error_Timestamp:
+                Last_SQL_Error_Timestamp:
+                        Source_SSL_Crl:
+                    Source_SSL_Crlpath:
+                    Retrieved_Gtid_Set:
+                        Executed_Gtid_Set:
+                            Auto_Position: 0
+                    Replicate_Rewrite_DB:
+                            Channel_Name:
+                    Source_TLS_Version:
+                Source_public_key_path:
+                    Get_Source_public_key: 0
+                        Network_Namespace:
+            1 row in set (0.00 sec)
+
+            ERROR:
+            No query specified
+
+        主库:
+            select * from tb_user;
+            +----+---------+------+
+            | id | name    | sex  |
+            +----+---------+------+
+            |  1 | Tom     | 1    |
+            |  2 | Trigger | 0    |
+            |  3 | Dawn    | 1    |
+            |  4 | Jerry   | 0    |
+            |  5 | Yubi    | 1    |
+            +----+---------+------+
+
+            insert into tb_user value(null, 'Butt', 0);     -- 主库添加数据
+            Query OK, 1 row affected (0.00 sec)
+
+            select * from tb_user;
+            +----+---------+------+
+            | id | name    | sex  |
+            +----+---------+------+
+            |  1 | Tom     | 1    |
+            |  2 | Trigger | 0    |
+            |  3 | Dawn    | 1    |
+            |  4 | Jerry   | 0    |
+            |  5 | Yubi    | 1    |
+            |  6 | Butt    | 0    |
+            +----+---------+------+
+
+        从库:
+            select * from tb_user;
+            +----+---------+------+
+            | id | name    | sex  |
+            +----+---------+------+
+            |  1 | Tom     | 1    |
+            |  2 | Trigger | 0    |
+            |  3 | Dawn    | 1    |
+            |  4 | Jerry   | 0    |
+            |  5 | Yubi    | 1    |
+            |  6 | Butt    | 0    |                         -- 右侧也会新添加数据
+            +----+---------+------+
